d43ddfafd6048ddceeb6fa2041c200c6aeb48ed8
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-01-08 20:41:22 -0200

    Aula 30. Express - Exercicio 03: Entendendo as diferente de usar server.use ou server.method

--- Fudamentos, Exercicos e anotacoes/Exercicios/express/ex03_express.js
+++ Fudamentos, Exercicos e anotacoes/Exercicios/express/ex03_express.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const server = express();
 
-server.get('/',function(req,res,next){
+server.use('/api',function(req,res,next){
     console.log('Início...');
     next();
     console.log('Fim...');
 });
 
-server.get('/',function(req,res,next){
-    console.log('Resposta...');
-    res.send('<h1>Olá Express</h1>')
+server.use('/api',function(req,res,next){
+    console.log('Respsota...');
+    res.send('<h1>API</h1>')
 });
 
 server.listen(3000,() =>{console.log('Executando...')});

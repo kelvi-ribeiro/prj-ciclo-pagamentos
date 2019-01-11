71c85a55c01967f0c1f1f2cbfee41d78ce1bb285
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-01-11 18:41:05 -0200

    Aula 47. Criando a conexao com o MongoDB e organizando arquivos

--- projeto-curso/backend/loader.js
+++ projeto-curso/backend/loader.js
@@ -1,16 +1,2 @@
-const port = 3003;
-
-const bodyParser = require('body-parser'),
-express = require('express'),
-server = express();
-
-server.use(bodyParser.urlencoded({extended:true}))
-server.use(bodyParser.json())
-
-server.listen(port,function(){
-    console.log(`BACKEND is running on port ${port}.`);
-});
-
-server.use(function(req,res,next){
-    res.send('Tudo Certo!!!');
-});
+require('./config/server')
+require('./config/database')

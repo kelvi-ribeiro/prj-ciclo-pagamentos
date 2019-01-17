8e34dbdcc1a4395b79675c7b3bd1757d2bea121b
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-01-17 11:44:44 -0200

    Correcao da task wach e correcao da tarefa server

--- projeto-curso/frontend/gulpTasks/server.js
+++ projeto-curso/frontend/gulpTasks/server.js
@@ -3,16 +3,16 @@ const watch = require('gulp-watch');
 const webserver = require('gulp-webserver');
 
 gulp.task('watch',()=>{
-    watch('app/**/*.html',gulp.start('app.html'))
-    watch('app/**/*.css',gulp.start('app.css'))
-    watch('app/**/*.js',gulp.start('app.js'))
+    watch('app/**/*.html',() => gulp.start('app.html'))
+    watch('app/**/*.css',() =>gulp.start('app.css'))
+    watch('app/**/*.js',() => gulp.start('app.js'))
     watch('assets/**/*.*', () => gulp.start('app.assets'))
 });
 
 gulp.task('server',['watch'],()=>{
     return gulp.src('public')
     .pipe(webserver({
-        liverealod:true,
+        livereload:true,
         port:3000,
         open:true
     }))

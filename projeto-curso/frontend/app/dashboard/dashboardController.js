dcca5f89292246fc3a89ead76f220dfd4dd535f4
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-01-20 03:17:38 -0200

    Aula 80. Aplicando dois Padroes do Angular Style Guide

--- projeto-curso/frontend/app/dashboard/dashboardController.js
+++ projeto-curso/frontend/app/dashboard/dashboardController.js
@@ -1,19 +1,21 @@
-angular.module('primeiraApp').controller('DashboardCtrl', [
-    '$scope',
+(function(){
+  angular.module('primeiraApp').controller('DashboardCtrl', [    
     '$http',
     DashboardController
   ])
   
-  function DashboardController($scope,$http) {
+  function DashboardController($http) {
    const url = 'http://localhost:3003/api/billingSummary';
-        $scope.getSummary = function(){
+   const vm = this
+        vm.getSummary = function(){
         $http.get(url).then(function(response) {
         const {credit = 0, debt = 0} = response.data
-        $scope.credit = credit
-        $scope.debt = debt
-        $scope.total = credit - debt        
+        vm.credit = credit
+        vm.debt = debt
+        vm.total = credit - debt        
       })
     }
   
-    $scope.getSummary()
+    vm.getSummary()
   }
+})();

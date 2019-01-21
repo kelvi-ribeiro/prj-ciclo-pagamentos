669bced8bfd6ee43b60c679cc8d99abea8d642a8
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-01-21 00:02:10 -0200

    Aula 86. Criacao e Utilizacao da Factory da Exibicao das mensagens

--- projeto-curso/frontend/app/billingCycle/billingCycleController.js
+++ projeto-curso/frontend/app/billingCycle/billingCycleController.js
@@ -1,16 +1,19 @@
 (function(){
     angular.module('primeiraApp').controller('BillingCycleCtrl',[
         '$http',
+        'msgs',
         BillingCycleController
     ])
-    function BillingCycleController($http){
+    function BillingCycleController($http,msgs){
         const vm = this;
         vm.create = function(){
             const url = 'http://localhost:3003/api/billingCycles';
             $http.post(url,vm.billingCycle)
             .then(function(response){
-                vm.billingCycle = {};
-                console.log('Sucesso!');
+                vm.billingCycle = {};                 
+                msgs.addSuccess(`Inclusão do ciclo de pagamento de ${response.data.name} Realizada com sucesso!!`);
+            }).catch(function(response){
+                msgs.addError(response.data.errors);
             })
         }
     }

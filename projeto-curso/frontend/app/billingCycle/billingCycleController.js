3091224e586deca3be61ba911122130c0f8ee38f
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-01-21 00:56:03 -0200

    Aula 90 Exibicao das abas de alterar e excluir

--- projeto-curso/frontend/app/billingCycle/billingCycleController.js
+++ projeto-curso/frontend/app/billingCycle/billingCycleController.js
@@ -25,6 +25,14 @@
                 msgs.addError(response.data.errors);
             })
         }
+        vm.showTabUpdate = function(billingCycle) {
+            vm.billingCycle = billingCycle;
+            tabs.show(vm,{tabUpdate:true}); 
+        }
+        vm.showTabDelete = function(billingCycle) {
+            vm.billingCycle = billingCycle;
+            tabs.show(vm,{tabDelete:true}); 
+        }
         vm.refresh();
     }
 })()

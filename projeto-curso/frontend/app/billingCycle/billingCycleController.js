277e4b611d21c92cfed4448cd0c52f02deaaa5a8
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-01-22 11:08:55 -0200

    Aula 100. Componente de Paginacao

--- projeto-curso/frontend/app/billingCycle/billingCycleController.js
+++ projeto-curso/frontend/app/billingCycle/billingCycleController.js
@@ -15,9 +15,9 @@
                 vm.billingCycle = {credits:[{}],debts:[{}]};
                 vm.billingCycles = response.data;
                 vm.calculateValues();
-                tabs.show(vm,{tabList: true, tabCreate: true})
                 $http.get(`${url}/count`).then(response => {
                     vm.pages = Math.ceil(response.data.value / 10)                    
+                    tabs.show(vm,{tabList: true, tabCreate: true})
                 })
             })
         }
